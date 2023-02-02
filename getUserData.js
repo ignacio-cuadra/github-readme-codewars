a46fdde3d5204b822f3d4f64ae14ff0c5c5d8980
Ignacio Cuadra <ignacio.cuadra.z@gmail.com>
2023-02-01 23:05:27 -0300

    feat: :sparkles: read name

--- getUserData.js
+++ getUserData.js
@@ -3,14 +3,17 @@ import { JSDOM } from "jsdom";
 const getUserData = (userId) => {
   return fetch(`https://www.codewars.com/users/${userId}`)
     .then((response) => response.text())
-    .then((html) => new JSDOM(html).window.document)
-    .then((document) => {
+    .then((html) => {
+      const document = new JSDOM(html).window.document;
       const data = {};
       document
-        .querySelectorAll(".stat-category .stat-box > .stat")
+        .querySelectorAll(
+          ".stat-category .stat-box > .stat, .user-profile .stat"
+        )
         .forEach((stat) => {
           const [rawKey, value] = stat.textContent.split(":");
           const key = {
+            Name: "name",
             Rank: "rank",
             Honor: "honor",
             "Leaderboard Position": "leaderboardPosition",
